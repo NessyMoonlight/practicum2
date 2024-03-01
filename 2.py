fc0dfe3d299cd0710227e8aba9e1fa8a52b95d12
NessyMoonlight <159246841+NessyMoonlight@users.noreply.github.com>
2024-03-02 03:32:46 +0700

    begin

---
+++ 2.py
@@ -0,0 +1,4 @@
+country_part1, country_part2 = \
+    (input('Введите название страны:').split())
+print(country_part1)
+print(country_part2)
---
+++ 3.py
@@ -0,0 +1,7 @@
+prices = input("Введите цены двух шоколадок через пробел: ").split()
+S = float(prices[0])
+R = float(prices[1])
+
+total_cost = S + R
+
+print("Общая стоимость двух шоколадок:", total_cost, "рублей")
